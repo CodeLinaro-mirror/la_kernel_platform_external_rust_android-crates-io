31c19cdd257522f3ee8c2263584a1699dc130166
James Farrell <jamesfarrell@google.com>
2026-05-13 17:44:45 +0000

    Update gdbstub to 0.7.10
    Bug: 495446228
    Test: treehugger
    Change-Id: Ica21df834a75d46527f474bd034793c46c7473a6

--- crates/gdbstub/src/arch.rs
+++ crates/gdbstub/src/arch.rs
@@ -41,6 +41,20 @@ pub trait RegId: Sized + Debug {
     ///
     /// Returns `None` if the register is not available.
     fn from_raw_id(id: usize) -> Option<(Self, Option<NonZeroUsize>)>;
+
+    /// Map a `RegId` back to a raw GDB register number.
+    ///
+    /// Returns `None` if this mapping direction is not implemented.
+    ///
+    /// This method currently only needs to return `Some` for a register if that
+    /// register is sent with
+    /// [`GdbStubStateMachineInner::report_stop_with_regs`].
+    ///
+    /// [`GdbStubStateMachineInner::report_stop_with_regs`]:
+    ///     crate::stub::state_machine::GdbStubStateMachineInner::report_stop_with_regs
+    fn to_raw_id(&self) -> Option<usize> {
+        None
+    }
 }
 
 /// Stub implementation -- Returns `None` for all raw IDs.
